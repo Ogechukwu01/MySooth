12fec0ed37f80165e8a8fdca4e67d2e9d611c3b6
OGECHUKWU NNENNA <eleodimuooge@gmail.com>
2024-01-06 10:46:52 +0100

    Week 9 Assessment

---
+++ app/src/main/java/com/codelab/basiclayouts/Week9Assessment.kt
@@ -0,0 +1,11 @@
+package com.codelab.basiclayouts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Week9Assessment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_week9_assessment)
+    }
+}
